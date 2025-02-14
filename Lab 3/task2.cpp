69fffb8069e28a37616ae8f96adbf9e31ee5effa
arafay20 <rafayparacha1620@gmail.com>
2025-02-14 20:05:27 +0500

    Update task2.cpp

--- Lab 3/task2.cpp
+++ Lab 3/task2.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class fitnesstracker {
+
+    string username;
+    int dailystepgoal;
+    int stepstaken;
+    double caloriesburned;
+
+public:
+    fitnesstracker(string name, int stepgoal) {
+        username = name;
+        dailystepgoal = stepgoal;
+        stepstaken = 0;
+        caloriesburned = 0.0;
+    }
+
+    
+    void logsteps(int steps) {
+        stepstaken += steps;
+        caloriesburned += steps * 0.04;
+        cout << "Steps logged successfully!" << endl;
+}
+
+    bool goalachieved() {
+        return stepstaken >= dailystepgoal;
+    }
+    
+    void displayprogress() {
+        cout << "User: " << username << endl;
+        cout << "daily step goal : " << dailystepgoal << endl;
+        cout << "Steps Taken: " << stepstaken <<  endl;
+        cout << "Calories Burned: " << caloriesburned << " kcal" << endl;
+        if (goalachieved()) {
+            cout << "Congratulations! You met your daily step goal!" << endl;
+        } else {
+            cout << "Keep going! You can do it!" << endl;
+        }
+    }
+};
+
+int main() {
+    
+    fitnesstracker tracker("Laiba", 10000);
+
+    tracker.logsteps(3000);
+    tracker.logsteps(5000);
+    tracker.logsteps(2500);
+    
+    tracker.displayprogress();
+    
+    return 0;
+}
 
