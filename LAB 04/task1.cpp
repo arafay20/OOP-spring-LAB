520dedd504fb2bab8d1163c48315b7000d969177
arafay20 <rafayparacha1620@gmail.com>
2025-02-18 20:49:12 +0500

    Update task1.cpp

--- LAB 04/task1.cpp
+++ LAB 04/task1.cpp
@@ -1,55 +1,58 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
+class Car {
+private:
+    string brand;
+    string model;
+    float price;
+    bool avail;
 
+public:
+    Car() {
+        brand = "Unknown";
+        model = "Generic";
+        price = 0.0;
+        avail = true;
+    }
+    
+    void setCar(string brandd, string m, float rent, bool a) {
+        brand = brandd;
+        model = m;
+        price = rent;
+        avail = a;
+    }
 
-class car{
-	private :
-	string brand;
-	string model;
-	float price;
-	bool avail;
-	
-	public :
-	
-	car(){
-		
-		brand ="unknown";
-		model = "generic";
-		price = 0;
-		avail = 1;
-	}
-	
-	
-	void setcar(string brandd, string m, float rent, bool a){
-		
-		brand =brandd;
-		model = m;
-		price = rent;
-		avail =  a;
-	}
-	
-	void request(){
-		
-		if(!avail){
-			cout << "not avialble, car already rented" << endl;
-		}else if(avail){
-			
-			cout << "car available , booked" << endl;
-			
-			
-			avail = 0;
-		}
-	}
+    void request(int days) {
+        if (!avail) {
+            cout << "Not available, car already rented." << endl;
+        } else {
+            
+            cout << "Car available, booked!" << endl;
+            cout << "Final Rent Price: $" << price << endl;
+            avail = false;  
+        }
+    }
+
+    
+    void display() {
+        cout << "Brand: " << brand << ", Model: " << model
+             << ", Price: $" << price << ", Available: " << (avail ? "Yes" : "No") << endl;
+    }
 };
 
-int main (){
-	
-	car c1;
-	
-	c1.request();
-	
-	
-	return 0;
+int main() {
+   
+    Car c1;
+    
+    c1.display();
+    c1.request(4);
+    c1.display(); 
+
+   
+
+   
+
+    return 0;
 }
+
